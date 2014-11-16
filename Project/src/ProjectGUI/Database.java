be4af36c29769be021af8ce58271d149d367f198
owen <owen@10.0.0.5>
2014-11-16 04:59:31 -0500

    database class: fetch methods return arraylists. i use these because of their flexibility and i figured we could call for all of them at the start in case calling all of the data one cell at a time caused lag and threw off the results. im going to try to implement them into our manager and various frames tonight. also if it doesnt automaticaly do it, you need to right click libraries and import jar file derby.jar. if you run a search im pretty sure it is included with glassfish

--- Project/src/ProjectGUI/Database.java
+++ Project/src/ProjectGUI/Database.java
@@ -11,11 +11,10 @@ import java.util.ArrayList;
 public class Database {
 	
 	final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	final String JDBC_URL = "jdbc:derby:outputDatabase;create=true";
+	final String JDBC_URL = "jdbc:derby:Database;create=true";
 	/* 
 	 * createTable should only execute when the program is initiated for the first time and
 	 * sets the tables to the default
-	 * I figure we can do this with a try statement to see if the tables return null or something
 	*/
 	public void createTables()throws ClassNotFoundException, SQLException
 	{
@@ -26,9 +25,46 @@ public class Database {
 		{
 		connection.createStatement().execute("create table passages(passage varchar(500))");
 		connection.createStatement().execute("insert into passages values " 
-				+ "('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),"
-				+ "('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'),"
-				+ "('ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc')");
+                                + "('An Overview of Personality Disorders'),"
+                                + "(''),"
+				+ "('We all think we know what a \"personality\" is. It\'s all of the characterisitic ways a person behaves'),"
+				+ "('and thinks: \"Michael tends to be shy\"; \" Mindy likes to be very dramatic\"; \"Juan is always),"
+                                + "('suspicious of others\"; \"Annette is very outgoing\"; \"Bruce seems to be very sensitive and gets),"
+                                + "('upset very easily over minor things”; “Sean has the personality of an eggplant!” We tent to type'),"
+                                + "('people as behaving in one way in many different situations. For example, like Michael, many of'),"
+                                + "('us are shy with people we don’t know, but we wont be shy around our friends. A truly shy'),"
+                                + "('person is shy even among people he or she has known for some time. The shyness is part of'),"
+                                + "('the way the person behaves in most situations. We have all probably behaved in all the ways'),"
+                                + "('noted here (“dramatic,” “suspicious,” “outgoing,” “easily upset”). However, we usually consider'),"
+                                + "('a way of behaving part of a person's personality only if it occurs in many times and places. In'),"
+                                + "('this chapter we look at characteristic ways of behaving in relation to personality disorders. First'),"
+                                + "('we examine in some detail how we conceptualize personality disorders and the issues related to'),"
+                                + "('them; then we describe the disorders themselves.'),"
+                                + "(''),"
+                                + "('Aspects of Personality Disorders'),"
+                                + "(''),"
+                                + "('What if a person's characteristic ways of thinking and behaving cause significant distress to the'),"
+                                + "('self or others? What if the person cant change this way of relating to the world and is unhappy?'),"
+                                + "('We might consider this person to have a “personality disorder.” The DSM-V definition notes'),"
+                                + "('that these personality characteristics are “inflexible and maladaptive and cause significant'),"
+                                + "('functional impairment of subjective distress.” Unlike many of the disorders we have already'),"
+                                + "('discussed, personality disorders are chronic; they do not come and go but originate in childhood'),"
+                                + "('and continue throughout adulthood. Because they affect personality, these chronic problems'),"
+                                + "('pervade every aspect of a person's life. If a man is overly suspicious, for example (a sign of a'),"
+                                + "('possible paranoid personality disorder), this trait will affect almost everything he does, including'),"
+                                + "('his employment (he may have to change jobs frequently if he believes co-workers conspire'),"
+                                + "('against him), his relationships (he may not be able to sustain a lasting relationship if he can't'),"
+                                + "('trust anyone), and even where he lives (he may have to move often if he suspects his landlord is out'),"
+                                + "('to get him).'),"
+                                + "(''),"
+                                + "('Paranoid personality disorders'),"
+                                + "(''),"
+                                + "('Although it is probably very adaptive to be a little wary of other people and their motives, being'),"
+                                + "('too distrustful can interfere with making friends, working with others, and, in general, getting'),"
+                                + "('through daily interactions in a functional way. People with paranoid personality disorder are'),"
+                                + "('excessively mistrustful and suspicious of others, without any justification. They assume other'),"
+                                + "('people are out to harm or trick them, and therefor they tend not to confide in others. Consider this'),"                     
+				+ "('case of Jake.')");
 		System.out.println("passages table created and records successfully inserted ...");
 		}
 		catch (SQLException e)
@@ -40,9 +76,11 @@ public class Database {
 		{
 		connection.createStatement().execute("create table SMS(message varchar(80))");
 		connection.createStatement().execute("insert into SMS values " 
-				+ "('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),"
-				+ "('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'),"
-				+ "('cccccccccccccccccccccccccccccccccccccccccccccccc')");
+				+ "('Tell me about your classes.'),"
+				+ "('Which classes have you had that youd recommend?'),"
+                                + "('What do you wanna do with your life when you get outta here?'),"
+                                + "('What do you like to do in your spare time?'),"
+				+ "('Had any trouble parking on campus?')");
 		System.out.println("SMS table created and records successfully inserted ...");
 		}
 		catch (SQLException e)
@@ -54,7 +92,12 @@ public class Database {
 		{
 		connection.createStatement().execute("create table readingQuestions(rquestion varchar(200), answer1 varchar(80), answer2 varchar(80), answer3 varchar(80), answer4 varchar(80), answer5 varchar(80))");
 		connection.createStatement().execute("insert into readingQuestions values " 
-				+ "('what is the capital of connecticut?', 'New Haven', 'Hartford', 'Bristol', 'New Amsterdam', 'Italy')");
+				+ "('Which of the following is not true about personality disorders?', 'are marked by distress to the individual experiencing them', 'pervade every aspect of a person's life, 'respond well to psychotherapy', 'tend to begin in childhood', ''),"
+                                + "('People with paranoid personality disorder', 'are very sensitive to criticism and have an excessive need for autonomy', 'lack of emotional expressiveness and pursue vague interests', 'find social isolation extremely painful', 'all of the above', ''),"
+                                + "('A person who seems \"aloof\", \"cold\" and \"indifferent\" to others but typically does not show signs of \"magical thinging\" is most likely to have which of the following disorders', 'schizoid personality disorder', 'paranoid personality disorder', 'schizotypal personality disorder', 'schizophrenic disorder', 'not enough information is given to be able to choose a best answer'),"
+                                + "('According to the reading, the treatment prospects for the people with schizoid personality disorder are:', 'quite good with biological therapies alone', 'quite good with psychotherapy alone', 'reasonably good when biological therapies are combined with psychological treatments for depression.', 'not good', 'none of the aboce is correct, the reading did not address treatment outcomes of persons with schizoid personality disorder'),"
+                                + "(Evidence for biological contributions to paranoid personality disorder is limited', 'True', 'False', '', '', ''),"
+                                + "('According to the reading, the term \"schizoid\" was used in the 1920\'s by', 'Bixler', 'Bleuler', 'Meehl', 'Stone', 'none of the above, the term schizoid was not used until the 1960\'s'),");
 		System.out.println("questions table created and records successfully inserted ...");
 		}
 		catch (SQLException e)
@@ -66,7 +109,13 @@ public class Database {
 		{
 		connection.createStatement().execute("create table demographicQuestions(dquestion varchar(200), answer1 varchar(80), answer2 varchar(80), answer3 varchar(80), answer4 varchar(80), answer5 varchar(80))");
 		connection.createStatement().execute("insert into demographicQuestions values " 
-				+ "('what is the capital of connecticut?', 'New Haven', 'Hartford', 'Bristol', 'New Amsterdam', 'Italy')");
+				+ "('I feel impatient when I read my textbooks.', 'Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree')"
+                                + "('I get absorbed in reading books for pleasure', 'Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree'),"
+                                + "('I get distracted easily when reading class assignments', 'Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree'),"
+                                + "('I rarely do the assigned readings for my classes', 'Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree'),"
+                                + "('I find it easy to focus on assigned readings', 'Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree'),"
+                                + "('Gender', 'Male', 'Female', '', '', ''),"
+                                + "('Age', '', '', '', '', '')");
 		System.out.println("questions table created and records successfully inserted ...");
 		}
 		catch (SQLException e)
