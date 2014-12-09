35f87d6f077c3222fd97fbac505b056ae77a9e96
owen <owen@10.0.0.5>
2014-12-09 11:27:20 -0500

    this version is better. everything is in the same columns. going to try to get it to sort by user, although unique ids would have been good

--- Project/src/ProjectGUI/ExportData.java
+++ Project/src/ProjectGUI/ExportData.java
@@ -24,7 +24,7 @@ public class ExportData {
     
     public static final String DATE_FORMAT_NOW = "yyyy-MM-dd-HH-mm-ss";
     private static String dbURL = "jdbc:derby://localhost:1527/sidresDB;create=true;user=sidresAdmin;password=1x!Software";
-    
+    private static String[] tableNames = {"PASSAGERESULTS", "QUESTIONRESULTS", "SMSRESULTS", "SURVEYRESULTS"};
     // Entrance point
   public static void export() throws ClassNotFoundException, SQLException {
     try{
@@ -80,7 +80,7 @@ public class ExportData {
       exOb.printStackTrace(System.out);
       return;
     }
-  }
+}
       public static String now() 
     {
     Calendar cal = Calendar.getInstance();
@@ -88,35 +88,95 @@ public class ExportData {
     return sdf.format(cal.getTime());
     }
 
- static ArrayList<String[]> getData()throws ClassNotFoundException, SQLException
-{
-    int numOfResultTables = 4;
-    String[] tableNames = {"PASSAGERESULTS", "QUESTIONRESULTS", "SMSRESULTS", "SURVEYRESULTS"};
-    ArrayList<String[]> message = new ArrayList<String[]>();
-    for(int table = 0; table < numOfResultTables; table ++)
+    static ArrayList<String[]> getData()throws ClassNotFoundException, SQLException
     {
+       int numOfResultTables = 4;
 
-        String SQL_STATEMENT = " select * from " + tableNames[table];
-        Connection connection = DriverManager.getConnection(dbURL);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(SQL_STATEMENT);
-        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-        int columnCount = resultSetMetaData.getColumnCount();
-        
- 
+       ArrayList<String[]> message = new ArrayList<String[]>();
+       for(int table = 0; table < numOfResultTables; table ++)
+       {
 
-        while (resultSet.next())
-        {
-            String[] entry = new String[columnCount];
-                for (int x = 1; x <= columnCount; x++)
-                        entry[x -1] = resultSet.getString(x);
-                message.add(entry);
-        }
-        if (statement != null) 
-                statement.close();
-        if (connection != null) 
-                connection.close();
-        }
+           String SQL_STATEMENT = " select * from " + tableNames[table];
+           Connection connection = DriverManager.getConnection(dbURL);
+           Statement statement = connection.createStatement();
+           ResultSet resultSet = statement.executeQuery(SQL_STATEMENT);
+           ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+           int columnCount = resultSetMetaData.getColumnCount();
+           int lines;
+           int ID;
+           
+           if(table == 0)
+           {
+               while (resultSet.next())
+               {
+                   String[] entry = new String[columnCount];
+
+                       entry[0] = resultSet.getString(2) + " line:" +resultSet.getString(3);
+                       entry[1] = resultSet.getString(5);
+                       entry[2] = resultSet.getString(4);
+                       message.add(entry);
+               }
+               if (statement != null) 
+                       statement.close();
+               if (connection != null) 
+                       connection.close();
+           }
+
+           if(table == 1)
+           {
+               while (resultSet.next())
+               {
+                   String[] entry = new String[columnCount];
+
+                       entry[0] = "Reading Questions| number:" + resultSet.getString(3);
+                       entry[1] = resultSet.getString(2);
+                       entry[2] = resultSet.getString(6);
+                       entry[3] = resultSet.getString(3);
+                       entry[4] = resultSet.getString(4);
+                       entry[5] = resultSet.getString(5);
+                       message.add(entry);
+               }
+               if (statement != null) 
+                       statement.close();
+               if (connection != null) 
+                       connection.close();
+           }
+           if(table == 2)
+           {
+               while (resultSet.next())
+               {
+                   String[] entry = new String[columnCount];
+                       entry[0] = "SMS";
+                       entry[1] = resultSet.getString(1);
+                       entry[2] = resultSet.getString(2);
+                       entry[3] = resultSet.getString(3);
+                       entry[4] = "word count:" + resultSet.getString(5) + " | response:" + resultSet.getString(4);
+                       message.add(entry);
+               }
+               if (statement != null) 
+                       statement.close();
+               if (connection != null) 
+                       connection.close();
+           }
+           if(table == 3)
+           {
+
+               while (resultSet.next())
+               {
+                   String[] entry = new String[columnCount];
+                       entry[0] = "Demographic Questions| number:" + resultSet.getString(3);
+                       entry[1] = resultSet.getString(2);
+                       entry[3] = resultSet.getString(3);
+                       entry[4] = resultSet.getString(5) + "" + resultSet.getString(4);
+                       message.add(entry);
+               }
+               if (statement != null) 
+                       statement.close();
+               if (connection != null) 
+                       connection.close();
+           }
+       }
         return message;
     }
 }
+  
